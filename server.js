5969c59c9e46bd747f6626efc06846829d3fcf93
ellaylone <heamik91@yandex.ru>
2016-08-08 21:35:07 +0300

    eslint, config-standard, 2 spaces

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-'use strict';
+'use strict'
 
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
 
-app.set('port', 9000);
+app.set('port', 9000)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+  console.time('render')
+  res.send('Hello World!')
+  console.timeEnd('render')
+})
 
 app.listen(app.get('port'), () => {
-    console.log(`Listening on port ${app.get('port')}`);
-});
+  console.log(`Listening on port ${app.get('port')}`)
+})
